64b22ae4f7d31d25d99df99bf61693688330c8be
Aditya <kathpalia.aditya11051999@gmail.com>
2020-04-27 01:52:51 +0530

    Functionality Setup complete

--- lib/main.dart
+++ lib/main.dart
@@ -1,20 +1,22 @@
 import 'package:flutter/material.dart';
-import 'package:spoton_music_player/screens/splash_screen.dart';
+import 'package:spoton_music_player/my_app.dart';
+import 'package:spoton_music_player/utils/themes.dart';
 
-void main() {
-  runApp(MyApp());
+void main() => runApp(new MyMaterialApp());
+
+class MyMaterialApp extends StatefulWidget {
+  @override
+  MyMaterialAppState createState() {
+    return new MyMaterialAppState();
+  }
 }
 
-class MyApp extends StatelessWidget {
-
+class MyMaterialAppState extends State<MyMaterialApp> {
   @override
   Widget build(BuildContext context) {
-    return MaterialApp(
-      debugShowCheckedModeBanner: false,
-      title: 'Flutter Demo',
-      routes: {
-        '/': (context) => SplashScreen(),
-      },
-    );
+    return new MaterialApp(
+        debugShowCheckedModeBanner: false,
+        theme: darkTheme,
+        home: new MyApp());
   }
 }
